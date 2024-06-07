70c61b71cf3c506396f8616f8905c01e4b76bebc
yojan david gil posada <yojan.david.gil.posada@gmail.com>
2024-06-07 14:39:06 -0500

    cambios 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
+import users from "../model/modelUsuario.js";
 //Ejecutando funciones
 document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
 document.getElementById("btn__registrarse").addEventListener("click", register);
 window.addEventListener("resize", anchoPagina);
 
-//Declarando variables
-var formulario_login = document.querySelector(".formulario__login");
-var formulario_register = document.querySelector(".formulario__register");
-var contenedor_login_register = document.querySelector(".contenedor__login-register");
-var caja_trasera_login = document.querySelector(".caja__trasera-login");
-var caja_trasera_register = document.querySelector(".caja__trasera-register");
 
-    //FUNCIONES
+
+
+
+//Declarando variables
+const formulario_login = document.querySelector(".formulario__login");
+const formulario_register = document.querySelector(".formulario__register");
+const contenedor_login_register = document.querySelector(".contenedor__login-register");
+const  caja_trasera_login = document.querySelector(".caja__trasera-login");
+const caja_trasera_register = document.querySelector(".caja__trasera-register");
+const  loginBtn = document.getElementById('btn-entrar');
+const  inputiniciar = document.getElementById('usuario');
+const  inputpassword = document.getElementById('contrasena');
+
+//FUNCIONES
+
 
 function anchoPagina(){
 
@@ -62,3 +71,39 @@ anchoPagina();
             caja_trasera_login.style.opacity = "1";
         }
 }
+
+
+    loginBtn.addEventListener('click',()=>{
+
+    if(usuario.value !== users.correo ){   
+        console.log('email no existe')
+
+    }
+
+    if(contrasena.value !== users.contrasena){
+        console.log('contraseña no existe')
+
+    }
+
+    if(usuario.value === users.correo && contrasena.value === users.contrasena){
+
+        window.location.href = "/home.html"
+
+
+
+
+    }
+
+}
+     
+   
+)
+
+    
+
+
+
+        
+    
+
+
---
+++ assets/model/modelUsuario.js
@@ -0,0 +1,10 @@
+const users = {
+
+    id: 1,
+    correo: "yojan.david.gil.posada@gmail.com",
+    contrasena: "nacional123"
+
+
+}
+
+export default users;
